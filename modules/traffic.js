690fe56f388c0aaa833e34f87e52ccde720480c8
OhradkaGergo <ohradkagergo@turr.hu>
2025-10-21 10:13:02 +0200

    traffic bugfix

--- modules/traffic.js
+++ modules/traffic.js
@@ -5,7 +5,7 @@ const pool = require('../utils/database')
 
 //select all traffic
 router.get('/', (req, res) => {
-    pool.query(`SELECT * FROM forgalom
+    pool.query(`SELECT forgalom.id, termek, vevo, kategoriaId, egyseg, nettoar, mennyiseg, kiadva, kategoriaNev FROM forgalom
         INNER JOIN kategoria on kategoria.id = forgalom.kategoriaId`, (error, results) => {
         if (error) return res.status(500).json({ error: error.message })
         res.status(200).json(results)
@@ -42,7 +42,8 @@ router.post('/', (req, res) => {
 router.patch('/:id', (req, res) => {
     let id = req.params.id
     const {termek, vevo, kategoriaId, egyseg, nettoar, mennyiseg, kiadva} = req.body
-    pool.query(`UPDATE kategoria SET termek=? vevo=? kategoriaId=? egyseg=? nettoar=? mennyiseg=? kiadva=? WHERE id=?`, [termek, vevo, kategoriaId, egyseg, nettoar, mennyiseg, kiadva, id], (error, results) => {
+    //UPDATE forgalom SET termek='Wawo', vevo='Owaw', kategoriaId=2, egyseg='dl', nettoar=510, mennyiseg=3, kiadva=1 WHERE forgalom.id=262
+    pool.query(`UPDATE forgalom SET termek=?, vevo=?, kategoriaId=?, egyseg=?, nettoar=?, mennyiseg=?, kiadva=? WHERE id=?`, [termek, vevo, kategoriaId, egyseg, nettoar, mennyiseg, kiadva, id], (error, results) => {
         if (error) return res.status(500).json({ error: error.message })
         res.status(200).json(results)
     })
